80a548be524557db7ba7b52ffc6094f4db6fb2fc
balamurugan-04 <67495513+balamurugan-04@users.noreply.github.com>
2022-09-14 21:33:52 +0530

    second commit

--- dist/better-onsubmit.js
+++ dist/better-onsubmit.js
@@ -1,47 +1,57 @@
+import swURL from "sw:../service-worker.js";
+
+// Register the service worker
+if ("serviceWorker" in navigator) {
+  // Wait for the 'load' event to not block other work
+  window.addEventListener("load", async () => {
+    // Try to register the service worker.
+    try {
+      const reg = await navigator.serviceWorker.register(swURL);
+      console.log("Service worker registered! 😎", reg);
+    } catch (err) {
+      console.log("😥 Service worker registration failed: ", err);
+    }
+  });
+}
 var betterOnsubmit = (function () {
-	'use strict';
+  "use strict";
 
-	var index = cb => e => {
-		e.preventDefault();
-		const form = e.target;
+  var index = (cb) => (e) => {
+    e.preventDefault();
+    const form = e.target;
 
-		if (!form) {
-			console.warn('No form detected by formie');
-			return
-		}
+    if (!form) {
+      console.warn("No form detected by formie");
+      return;
+    }
 
-		const rawChildren = form.querySelectorAll('*');
-		const children = Array.prototype.slice.call(rawChildren);
-		const inputs = children.filter(node => node.type === 'radio'
-			? node.checked
-			: Boolean(node.name)
-		);
+    const rawChildren = form.querySelectorAll("*");
+    const children = Array.prototype.slice.call(rawChildren);
+    const inputs = children.filter((node) => (node.type === "radio" ? node.checked : Boolean(node.name)));
 
-		let result = {};
+    let result = {};
 
-		inputs.forEach(input => {
-			let value;
+    inputs.forEach((input) => {
+      let value;
 
-			if (input.type === 'checkbox') {
-				value = input.checked;
-			} else if (input.type === 'file') {
-				value = input.files;
-			} else if (input.type === 'select-multiple') {
-				value = Array.prototype.slice.call(input.options).filter(
-					option => option.selected
-				).map(
-					option => option.value
-				);
-			} else {
-				value = input.value;
-			}
+      if (input.type === "checkbox") {
+        value = input.checked;
+      } else if (input.type === "file") {
+        value = input.files;
+      } else if (input.type === "select-multiple") {
+        value = Array.prototype.slice
+          .call(input.options)
+          .filter((option) => option.selected)
+          .map((option) => option.value);
+      } else {
+        value = input.value;
+      }
 
-			result[input.name] = value;
-		});
+      result[input.name] = value;
+    });
 
-		cb(result);
-	};
+    cb(result);
+  };
 
-	return index;
-
-}());
+  return index;
+})();
--- src/index.js
+++ src/index.js
@@ -1,40 +1,51 @@
-export default cb => e => {
-	e.preventDefault()
-	const form = e.target
+import swURL from "sw:../service-worker.js";
 
-	if (!form) {
-		console.warn('No form detected by formie')
-		return
-	}
-
-	const rawChildren = form.querySelectorAll('*')
-	const children = Array.prototype.slice.call(rawChildren)
-	const inputs = children.filter(node => node.type === 'radio'
-		? node.checked
-		: Boolean(node.name)
-	)
-
-	let result = {}
-
-	inputs.forEach(input => {
-		let value;
-
-		if (input.type === 'checkbox') {
-			value = input.checked
-		} else if (input.type === 'file') {
-			value = input.files
-		} else if (input.type === 'select-multiple') {
-			value = Array.prototype.slice.call(input.options).filter(
-				option => option.selected
-			).map(
-				option => option.value
-			)
-		} else {
-			value = input.value
-		}
-
-		result[input.name] = value
-	})
-
-	cb(result)
+// Register the service worker
+if ("serviceWorker" in navigator) {
+  // Wait for the 'load' event to not block other work
+  window.addEventListener("load", async () => {
+    // Try to register the service worker.
+    try {
+      const reg = await navigator.serviceWorker.register(swURL);
+      console.log("Service worker registered! 😎", reg);
+    } catch (err) {
+      console.log("😥 Service worker registration failed: ", err);
+    }
+  });
 }
+export default (cb) => (e) => {
+  e.preventDefault();
+  const form = e.target;
+
+  if (!form) {
+    console.warn("No form detected by formie");
+    return;
+  }
+
+  const rawChildren = form.querySelectorAll("*");
+  const children = Array.prototype.slice.call(rawChildren);
+  const inputs = children.filter((node) => (node.type === "radio" ? node.checked : Boolean(node.name)));
+
+  let result = {};
+
+  inputs.forEach((input) => {
+    let value;
+
+    if (input.type === "checkbox") {
+      value = input.checked;
+    } else if (input.type === "file") {
+      value = input.files;
+    } else if (input.type === "select-multiple") {
+      value = Array.prototype.slice
+        .call(input.options)
+        .filter((option) => option.selected)
+        .map((option) => option.value);
+    } else {
+      value = input.value;
+    }
+
+    result[input.name] = value;
+  });
+
+  cb(result);
+};
